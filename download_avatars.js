7d57a16da85ad6d66bf2b24a6745a58c1867737c
Bernardo Siqueira <bernardofcs@hotmail.com>
2017-02-13 21:57:42 +0000

    added error handling

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,6 @@
 require('dotenv').config();
 var request = require("request");
 var fs = require('fs');
-
-if(!process.argv[3]){
-  throw new Error('2 arguments required');  // Checks if there are 2 arguments from the command line
-}
-
-
 var GITHUB_USER = process.env.GITHUB_USER;
 var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
@@ -30,17 +24,48 @@ function getRepoContributors(repoOwner, repoName, cb) { // gets the list of avat
   }
   request.get(options, function(error, response, body){   // parses the body json object and sends to the callback
     var obj = JSON.parse(body);
-    cb(error, obj);
+    cb(error, obj);                                       //callback function called
   })
   .on('error', function(err) {
     throw err;
   });
 }
 
-getRepoContributors(process.argv[2], process.argv[3], function(err, result){
-  for(var user of result){
-    downloadImageByURL(user['avatar_url'], 'avatars/' + user['login']);   //saves each avatar image from a repo into a custom file
-  }
-})
+function checkEnv(){ // check if .env file exists
+  return fs.existsSync('.env');
+}
 
+function checkEnvInfo(){
+  if (process.env.GITHUB_USER && process.env.GITHUB_TOKEN){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+if(process.argv.length === 4){ // Checks if there are 2 arguments from the command line
+  if(checkEnv){                // Checks if .env file exists
+    if(checkEnvInfo){          // Checks if .env file has github user and token
+      getRepoContributors(process.argv[2], process.argv[3], function(err, result){ //main program flow
+        var count = 0;
+        for(var user of result){
+          count++;
+          var dir = './avatars';        //target directory
+          if(!fs.existsSync(dir)){      //checks if dir exists and creates it if it doesn't
+            fs.mkdir(dir);
+          }
+          downloadImageByURL(user['avatar_url'], dir + '/' + user['login']);   //saves each avatar image from a repo into a custom file
+          console.log(count + ' image(s) added to the ' + dir + ' folder.');
+        }
+      });
+    }else{
+      console.log('.env file is missing information.');
+    }
+  }else{
+    console.log('.env file does not exist');
+  }
+}else{
+  console.log('Exactly 2 arguments are required: GitHub User and Repo Name');
+}
 
