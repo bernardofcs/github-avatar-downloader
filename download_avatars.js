8ab374be3acd2c719891d09f7a75ce99e41d1365
Bernardo Siqueira <bernardofcs@hotmail.com>
2017-02-13 18:41:47 +0000

    created getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 var request = require("request");
+var fs = require('fs');
 
-console.log("Welcome to the GitHub Avatar Downloader!");
+function getRepoContributors(repoOwner, repoName, cb) {
+  request.get('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
+    .on('error', function (err) {                                   // Note 2
+      throw err;
+    })
+    .on('response', function(response){
+      cb(response);
+    })
+}
+
+getRepoContributors('jquery', 'jquery', function(err, result){
+  console.log('Errors: ' + err;
+  console.log('Result: ' + result);
+})
